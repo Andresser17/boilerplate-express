0e4188e1b9bc98e2fada07aef629416b35b17061
Andresser17 <tensasangetsu1254@gmail.com>
2021-12-10 14:26:09 -0400

    get back to commit 46db484bf0e96e29e8db46c02771b926bb8a82ca

--- myApp.js
+++ myApp.js
@@ -1,19 +1,14 @@
-// require("dotenv").config();
 var express = require("express");
 var app = express();
 
 app.get("/", (req, res) => {
   // res.send("Hello Express")
+  console.log(`${__dirname}/views/index.html`);
   res.sendFile(`${__dirname}/views/index.html`);
 });
 
 app.get("/json", (req, res) => {
-  // if (process.env.MESSAGE_STYLE == "uppercase") {
-  //   res.json({ message: "HELLO JSON" });
-  // } else {
-  //   res.json({ message: "Hello json" });
-  // }
-    res.json({ message: "Hello json" });
+  res.json({ message: "Hello json" });
 });
 
 app.use("/public", express.static(`${__dirname}/public`));
