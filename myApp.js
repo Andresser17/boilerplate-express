6f4f5a4e476ae6f4ffee7ace1723595879607028
Andresser17 <tensasangetsu1254@gmail.com>
2021-12-10 10:34:19 -0400

    Response index.html in / path

--- myApp.js
+++ myApp.js
@@ -2,7 +2,9 @@ var express = require("express");
 var app = express();
 
 app.get("/", (req, res) => {
-  res.send("Hello Express")
+  // res.send("Hello Express")
+  console.log(`${__dirname}/views/index.html`)
+  res.sendFile(`${__dirname}/views/index.html`)
 })
 
 module.exports = app;
