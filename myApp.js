23eeebf6d5208bdaa6ae0e7e2033f0ad52353762
Andresser17 <tensasangetsu1254@gmail.com>
2021-12-10 13:58:24 -0400

    removed changes

--- myApp.js
+++ myApp.js
@@ -1,4 +1,4 @@
-require("dotenv").config();
+// require("dotenv").config();
 var express = require("express");
 var app = express();
 
@@ -8,11 +8,12 @@ app.get("/", (req, res) => {
 });
 
 app.get("/json", (req, res) => {
-  if (process.env.MESSAGE_STYLE == "uppercase") {
-    res.json({ message: "HELLO JSON" });
-  } else {
+  // if (process.env.MESSAGE_STYLE == "uppercase") {
+  //   res.json({ message: "HELLO JSON" });
+  // } else {
+  //   res.json({ message: "Hello json" });
+  // }
     res.json({ message: "Hello json" });
-  }
 });
 
 app.use("/public", express.static(`${__dirname}/public`));
