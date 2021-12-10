46db484bf0e96e29e8db46c02771b926bb8a82ca
Andresser17 <tensasangetsu1254@gmail.com>
2021-12-10 11:43:07 -0400

    change json response in /json endpoint

--- myApp.js
+++ myApp.js
@@ -8,7 +8,7 @@ app.get("/", (req, res) => {
 });
 
 app.get("/json", (req, res) => {
-  res.json({ message: "Hello JSON" });
+  res.json({ message: "Hello json" });
 });
 
 app.use("/public", express.static(`${__dirname}/public`));
