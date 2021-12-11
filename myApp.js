64167660b0d10a6c42aa36d526e6d9362537fe52
Andresser17 <andresserserrano@gmail.com>
2021-12-10 22:11:19 -0400

    Change /json endpoint res

--- myApp.js
+++ myApp.js
@@ -1,4 +1,4 @@
-require("dotenv").config();
+// require("dotenv").config();
 var express = require("express");
 var app = express();
 
@@ -9,11 +9,13 @@ app.get("/", (req, res) => {
 });
 
 app.get("/json", (req, res) => {
-  if (process.env.MESSAGE_STYLE === "uppercase") {
-    res.json({ message: "HELLO JSON" });
-  } else {
-    res.json({ message: "Hello json" });
-  }
+  // process.env.MESSAGE_STYLE = "uppercase"
+  // if (process.env.MESSAGE_STYLE === "uppercase") {
+  //   res.json({ message: "HELLO JSON" });
+  // } else {
+  //   res.json({ message: "Hello json" });
+  // }
+  res.send(process.env.MESSAGE_STYLE)
 });
 
 app.use("/public", express.static(`${__dirname}/public`));
