c8d620f981018628c6cf95e050dd3e8e97951089
Andresser17 <andresserserrano@gmail.com>
2021-12-10 22:51:08 -0400

    get back to commit (46db484bf0e96e29e8db46c02771b926bb8a82ca)

--- myApp.js
+++ myApp.js
@@ -2,12 +2,13 @@ var express = require("express");
 var app = express();
 
 app.get("/", (req, res) => {
+  // res.send("Hello Express")
   console.log(`${__dirname}/views/index.html`);
   res.sendFile(`${__dirname}/views/index.html`);
 });
 
 app.get("/json", (req, res) => {
-  res.send("Hello World");
+  res.json({ message: "Hello json" });
 });
 
 app.use("/public", express.static(`${__dirname}/public`));
